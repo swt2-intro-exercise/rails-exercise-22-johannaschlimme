0c581a6d75dab3ad316dd0ff65cb0d58c41b1031
johannaschlimme <johanna.schlimme@gmx.de>
2022-11-08 22:55:01 +0100

    Failing test for issue #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Author model", type: :model do
+    it "should have a first_name, last_name and homepage, which are all strings" do
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author.first_name).to eq('Alan')
+        expect(author.last_name).to eq('Turing')
+        expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    end
+
+    it "should have a method name (no parameters) which returns the full name of an author" do
+        author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+        expect(author.name()).to eq('Alan Turing')
+    end
+end
